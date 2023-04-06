83febd803bc7003ef60b81ecdf8251d387a24f56
o-ddong <odh0112@naver.com>
2023-04-06 21:53:30 +0900

    Feat: 존재하는 모델 admin 추가

--- applications/board/admin.py
+++ applications/board/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.board.models import Notice, Post, Comment, Scrap
+
+admin.site.register(Notice)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Scrap)
+
+
+
+
+
--- applications/cert/admin.py
+++ applications/cert/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.cert.models import University, User
+
+admin.site.register(University)
+admin.site.register(User)
